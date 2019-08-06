1870455584c8ad2f269be16daaa52320e9baeea5
creanme <creanme@live.com>
2019-08-06 11:05:22 +0800

    chore: 去除顶部随机的图片

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -49,6 +49,12 @@ module.exports = {
         // 上方 header 的相关设置
         header: {
             // header 的背景，可以使用图片，或者随机变化的图案（geopattern）
+            background: {
+                // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
+                // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
+                // url: 'https://wx3.sinaimg.cn/large/6dd11bf2gy1g5prn13cl6j20xc0gt78i.jpg',
+                useGeo: false
+            },
 
             // 是否在 header 显示标题
             showTitle: true
