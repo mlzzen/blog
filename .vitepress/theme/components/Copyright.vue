e5aceed333376c5aec83854b8b36db4f4b870991
mlzzen <mlzzen@qq.com>
2025-08-20 16:21:45 +0800

    style: 格式化Copyright.vue组件的模板和脚本

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="site-footer">
-        MIT Licensed | Copyright © 2021-2022 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.1.4</a>
-        Theme by <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">Vitepress-blog</a>
+        MIT Licensed | Copyright © 2021-2022
+        <a class="vitepress" :href="website">{{ webTitle }}</a>
+        <br />
+        Powered by
+        <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress</a>
+        Theme by
+        <a class="vitepress" target="_blank" href="//github.com/airene/vitepress-blog-pure">
+            Vitepress-blog
+        </a>
     </div>
 </template>
 <script lang="ts" setup>
-import { useData } from 'vitepress'
+import { useData } from 'vitepress';
 
-const { site, theme } = useData()
-const website = theme.value.website
-const webTitle = site.value.title
+const { site, theme } = useData();
+const website = theme.value.website;
+const webTitle = site.value.title;
 </script>
 
 <style>
